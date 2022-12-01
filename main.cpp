816890bf2d6557e00a41640d1b46df35cbfe774c
zinkb <zinkb@umich.edu>
2022-12-01 13:06:56 -0500

    start of main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+class Classifier {
+    private :
+
+
+    public :
+
+
+};
+
+int main(int argc, char *argv[]) {
+
+
+
+}
