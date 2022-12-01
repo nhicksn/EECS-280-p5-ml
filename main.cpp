26868703d4e1e18d9077ea652ce7429b00b4e7d1
zinkb <zinkb@umich.edu>
2022-12-01 13:19:36 -0500

    read in files

--- main.cpp
+++ main.cpp
@@ -16,6 +16,31 @@ class Classifier {
 
 int main(int argc, char *argv[]) {
 
+//error checking
+if (argc != 3 || argc != 4){
+    cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
+}
 
+if( argv[3] != ""){
+    if (argv[3] != "--debug"){
+        cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
+    }
+}
+
+string trainFileName = argv[1];
+string testFileName = argv[2];
+
+// read in input files
+ifstream finTrain;
+finTrain.open(trainFileName);
+
+ifstream finTest;
+finTest.open(testFileName);
+
+    // check for errors
+if (!finTrain.is_open() || !finTest.is_open()) {
+    cout << "Error opening file: " << trainFileName << endl;
+    return 1;
+}
 
 }
