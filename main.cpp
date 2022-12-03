68642af6ed08289c93e0a60a14ee9453fed54c94
nhicksn <nhicksn@umich.edu>
2022-12-03 17:07:07 -0500

    a

--- main.cpp
+++ main.cpp
@@ -1,17 +1,106 @@
+// Project UID db1f506d06d84ab787baf250c265e24e
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cassert>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <string.h>
 
 using namespace std;
 
 class Classifier {
     private :
-
-
+        int totalPosts;
+        int uniqueWords;
+        map<string, int> numPostsWithWord;
+        map<string, int> numPostsWithLabel;
+        map<pair<string, string>, int> numPostsWithLabelWithWord;
+        
     public :
+        Classifier() :
+            totalPosts(0), uniqueWords(0) { }
+        
+        void countPosts(ifstream train) {
+            string garbage;
+            getline(train, garbage);
+            train >> totalPosts;
+        }
 
+        void countWords(ifstream train) {
+            string garbage;
+            getline(train, garbage);
+            map<string, bool> uniqueWord;
+            string trash1, trash2, trash3;
+            vector<string> words;
+            for(int i = 0; i < totalPosts; i++) {
+                train >> trash1 >> trash2 >> trash3;
+                getline(train, words[i]);
+            }
+            for(int i = 0; i < totalPosts; i++) {
+                string word;
+                istringstream wordStream(words[i]);
+                while(wordStream >> word) {
+                    if(uniqueWord[word] == false) {
+                    uniqueWords++;
+                    uniqueWord[word] = true;
+                    }
+                }                
+            }
+        }
 
+        void numPostsWord(ifstream train, string inputWord) {
+            string garbage;
+            getline(train, garbage);
+            string trash1, trash2, trash3;
+            vector<string> words;
+            for(int i = 0; i < totalPosts; i++) {
+                train >> trash1 >> trash2 >> trash3;
+                getline(train, words[i]);
+            }
+            for(int i = 0; i < totalPosts; i++) {
+                string word;
+                istringstream wordStream(words[i]);
+                while(wordStream >> word) {
+                    if(inputWord == word) {
+                        numPostsWithWord[inputWord]++;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        void numPostsLabel(ifstream train, string inputLabel) {
+            string garbage;
+            getline(train, garbage);
+            string trash1, trash2, trash3, label;
+            for(int i = 0; i < totalPosts; i++) {
+                train >> trash1 >> trash2 >> label >> trash3;
+                if(inputLabel == label) {
+                    numPostsWithLabel[inputLabel]++;
+                }
+            }
+        }
+
+        void numPostsWordLabel(ifstream train, string inputWord, string inputLabel) {
+            string garbage;
+            getline(train, garbage);
+            string trash1, trash2, label, words, wordCheck;
+            for(int i = 0; i < totalPosts; i++) {
+                train >> trash1 >> trash2 >> label >> words;
+                if(label == inputLabel) {
+                    stringstream wordStream(words);
+                    while(wordStream >> wordCheck) {
+                        if(wordCheck == inputWord) {
+                            numPostsWithLabelWithWord[{inputLabel, inputWord}]++;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
 };
 
 int main(int argc, char *argv[]) {
@@ -22,7 +111,7 @@ if (argc != 3 || argc != 4){
 }
 
 if( argc == 4){
-    if (argv[3] != "--debug"){
+    if (!strcmp(argv[3], "--debug")){
         cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
     }
 }
@@ -30,7 +119,7 @@ if( argc == 4){
 //Debugger/Output Toggle
 bool debugToggle = false;
 
-if(argc = 4){
+if(argc == 4){
     debugToggle = true;
 }
 
@@ -52,6 +141,7 @@ if (!finTrain.is_open() || !finTest.is_open()) {
 
 //NOTES / Outline
 //read posts from train file
+
 
 //train the classifier and store information needed
 //total number of posts - int
